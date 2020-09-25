11da4c16070ea065f1df491181c2b4a2f677b17e
QinCalvin <calvin.qin.ca@gmail.com>
2020-09-25 16:19:16 -0700

    Solved Towers of Hanoi

---
+++ solutions/hanoi.py
@@ -0,0 +1,40 @@
+class Tower:
+    def __init__(self, n):
+        self.one = [x for x in range(n)]
+        self.two = []
+        self.three = []
+        self.size = n
+
+    def pTowers(self):
+        print("---pTowers---")
+        print("One: ", self.one)
+        print("Two: ", self.two)
+        print("Three: ", self.three)
+        print()
+    
+    def _verify_stack(self, stack):
+        return all(stack[i] < stack[i+1] for i in range(len(stack)-1))
+    
+    def verify(self):
+        return self._verify_stack(self.one) and self._verify_stack(self.two) and self._verify_stack(self.three)
+
+    def hanoi(self, start, end, temp, amount):
+        if not self.verify():
+            self.pTowers()
+            assert False
+        if amount == 1:
+            end.append(start.pop())
+        else:
+            self.hanoi(start, temp, end, amount-1)
+            end.append(start.pop())
+            self.hanoi(temp, end, start, amount-1)
+
+    def solve(self):
+        self.hanoi(self.one, self.three, self.two, self.size)
+
+
+
+t = Tower(4)
+t.pTowers()
+t.solve()
+t.pTowers()
